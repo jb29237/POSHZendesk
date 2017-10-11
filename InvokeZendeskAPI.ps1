0909926192403f23f89b41268f157f0751bd1a61
milkshakespoon <adamgentle@hotmail.co.uk>
2017-10-11 20:40:26 +0100

    API call helper function initial commit. Still needs work and testing.

---
+++ InvokeZendeskAPI.ps1
@@ -0,0 +1,20 @@
+﻿#Currently the username must be specified like this - "example@domain.com/token" - We should probably add the token part as a string to the end of the email.
+
+function InvokeZendeskAPI
+{
+
+    Param
+    (
+        [string]$InstanceURL,
+        [string]$APIEndpoint,
+        [string]$Method,
+        [string]$Username,
+        [string]$Token
+    )
+
+    $Headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($Username):$($Token)"));} 
+
+    $Output = Invoke-RestMethod -Uri ($InstanceURL+$APIEndpoint) -Method $Method -Headers $Headers -ContentType "application/json"
+    return $Output
+    
+}

c4e11215f9839a3a654e4a10fec450c078882a2b
milkshakespoon <adamgentle@hotmail.co.uk>
2017-11-03 09:58:38 +0000

    Adding new dot sourcing for testing.

--- POSHZendesk.psm1
+++ POSHZendesk.psm1
@@ -1,3 +1,15 @@
-﻿& . .\*.ps1
-
-Export-ModuleMember -Function *-*
+﻿$functions  = Get-ChildItem -Path "$PSScriptRoot\*.ps1" -Recurse -ErrorAction SilentlyContinue
+ 
+ #Dot source the files
+   Foreach($import in $functions)
+   {
+       Try
+       {
+           . $import.fullname
+           
+       }
+       Catch
+       {
+           Write-Error -Message "Failed to import function $($import.fullname): $_"
+       }
+   }
